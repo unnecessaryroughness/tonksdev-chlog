04b72e828562afdb8937fc9162ba64024cb4225f
unnecessaryroughness <marktonks75@gmail.com>
2015-07-17 21:11:54 +0100

    integrated sass into the project

--- common/templates/mainmenu.js
+++ common/templates/mainmenu.js
@@ -4,21 +4,33 @@ function toggleMenu() {
 }
 
 function openMegaMenu() {
-    $(".chlog-submenu").hide();
+    if (screen.width >= 620) {
+        $(".chlog-submenu").fadeOut(400);
+    } else {
+        $(".chlog-submenu").hide();
+    }
     $("#chlog-mainmenu>ul").css("border-bottom", "1px solid #666");
-    $("#chlog-mainmenu>ul>li").css("background-color", "#fff");
+    $("#chlog-mainmenu>ul>li").css("background-color", "#fff").css("color", "#666");
     
     if ($(this).attr("submenu") != undefined) {
-        $(this).css("background-color", "#eef");
+        $(this).css("background-color", "#0775ba").css("color", "#fff");
         $("#chlog-mainmenu>ul").css("border-bottom", "0px");
-        $("#"+$(this).attr("submenu")).show();
+        if (screen.width >= 620) {
+            $("#"+$(this).attr("submenu")).fadeIn(400);
+        } else {
+            $("#"+$(this).attr("submenu")).show();
+        }
     } 
 }
 
 function closeMegaMenu() {
     $("#chlog-mainmenu>ul").css("border-bottom", "1px solid #666");
-    $("#chlog-mainmenu>ul>li").css("background-color", "#fff");
-    $(this).hide();   
+    $("#chlog-mainmenu>ul>li").css("background-color", "#fff").css("color", "#666");
+    if (screen.width >= 620) {
+        $(this).fadeOut(400);   
+    } else {
+        $(this).hide();   
+    }
 }
 
 
